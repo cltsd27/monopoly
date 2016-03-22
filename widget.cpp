d0c475be10094e07289fa3bf38d9e235e9409294
Christopher Mills <christophermills@localhost.localdomain>
2016-03-21 23:19:14 -0400

    fully implemented add player and remove player buttons

--- widget.cpp
+++ widget.cpp
@@ -25,40 +25,44 @@ Widget::~Widget()
 
 void Widget::on_removePl_clicked()
 {
-    if(ui->playerFour->isVisible()){
+    if(ui->playerSix->isVisible()){
+        ui->playerSix->setVisible(false);
+        ui->plSixMoney->setVisible(false);
+        ui->addPlayer->setEnabled(true);
+    }
+    else if(ui->playerFive->isVisible()){
+        ui->playerFive->setVisible(false);
+        ui->plFiveMoney->setVisible(false);
+    }
+    else if(ui->playerFour->isVisible()){
         ui->playerFour->setVisible(false);
         ui->plFourMoney->setVisible(false);
     }
     else if(ui->playerThree->isVisible()){
         ui->playerThree->setVisible(false);
         ui->plThreeMoney->setVisible(false);
-    }
-    else if(ui->playerTwo->isVisible()){
-        ui->playerTwo->setVisible(false);
-        ui->plTwoMoney->setVisible(false);
-    }
-    else if(ui->playerOne->isVisible()){
-        ui->playerOne->setVisible(false);
-        ui->plOneMoney->setVisible(false);
+        ui->removePl->setEnabled(false);
     }
 }
 
 void Widget::on_addPlayer_clicked()
 {
-    if(!ui->playerOne->isVisible()){
-           ui->playerOne->setVisible(true);
-           ui->plOneMoney->setVisible(true);
-       }
-    else if(!ui->playerTwo->isVisible()){
-        ui->playerTwo->setVisible(true);
-        ui->plTwoMoney->setVisible(true);
-    }
-    else if(!ui->playerThree->isVisible()){
+    if(!ui->playerThree->isVisible()){
         ui->playerThree->setVisible(true);
         ui->plThreeMoney->setVisible(true);
+        ui->removePl->setEnabled(true);
     }
     else if(!ui->playerFour->isVisible()){
         ui->playerFour->setVisible(true);
         ui->plFourMoney->setVisible(true);
+    }
+    else if(!ui->playerFive->isVisible()){
+        ui->playerFive->setVisible(true);
+        ui->plFiveMoney->setVisible(true);
+    }
+    else if(!ui->playerSix->isVisible()){
+        ui->playerSix->setVisible(true);
+        ui->plSixMoney->setVisible(true);
+        ui->addPlayer->setEnabled(false);
     }
 }
